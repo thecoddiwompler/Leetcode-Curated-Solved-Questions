ab37581f0412ec5fe50c7f6a449ad1e21998aaef
Indrajeet Mishra <mindra673@gmail.com>
2023-08-01 12:36:26 +0530

    Create solution2789.py

---
+++ Solutions/solution2789.py
@@ -0,0 +1,24 @@
+class Solution:
+    def maxArrayValue(self, nums: List[int]) -> int:
+        nums.reverse()
+        start=0
+        ans=0
+        while True:
+            temp=nums[start]
+            break_flag=1
+            for i in range(start,len(nums)-1):
+                if nums[i+1]<=temp:
+                    temp+=nums[i+1]
+                else:
+                    if temp>ans:
+                        ans=temp
+                    start=i+1
+                    break_flag=0
+                    break
+            if start==len(nums)-1 or break_flag==1:
+                if temp>ans:
+                    ans=temp
+                if nums[-1]>ans:
+                    ans=nums[-1]
+                break
+        return ans
